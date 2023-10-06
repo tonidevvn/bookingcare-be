30080f14f7eed2e70cc66d8ab73b56465f73ddb0
tonidevvn <tonidevvn@gmail.com>
2023-10-06 18:14:45 +0700

    set up sequelize and sequelize-cli

--- src/server.js
+++ src/server.js
@@ -1,16 +1,13 @@
+import "dotenv/config";
+import { initWebRoutes } from "./route/web";
 const express = require("express");
+
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 6969;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello new world!");
-});
-
-app.get("/toni", (req, res) => {
-  res.send("Hello Toni 12!");
-});
+initWebRoutes(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
